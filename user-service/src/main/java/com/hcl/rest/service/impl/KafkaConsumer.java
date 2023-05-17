f0949815d49a5d73ea731130135896f042ee89ee
susanthb <susanthb@hcl.com>
2023-05-17 13:17:23 +0530

    ChatGPT-Commit-fe5a2151-d469-4599-bc64-ebaa9968ba36

--- user-service/src/main/java/com/hcl/rest/service/impl/KafkaConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.hcl.rest.service.impl;
-@Component
-public class KafkaConsumer {
-    @KafkaListener(topics = "userupdate")
-    public void consumeMessage(User user) {
-        // Update user status
-    }
-}
