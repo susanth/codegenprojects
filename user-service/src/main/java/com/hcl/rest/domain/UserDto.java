0f826bd5b6db34a34b6ccdae9947634b8f0359b6
susanthb <susanthb@hcl.com>
2023-05-18 14:51:40 +0530

    ChatGPT-Commit-9072216c-04f4-4071-85a5-8a86a8a88d7c

--- user-service/src/main/java/com/hcl/rest/domain/UserDto.java
+++
@@ -1,8 +0,0 @@
-package com.hcl.rest.domain;
-public class UserDto {
-    private int a;
-    private String b;
-    private String status;
-    
-    // Getters and Setters
-}
--- user-service/src/main/java/com/hcl/rest/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hcl.rest.repository;
-import com.hcl.rest.entity.User;
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
